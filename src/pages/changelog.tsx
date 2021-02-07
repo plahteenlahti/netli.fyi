54aa1970b299961d5ebd8408812c679259dcfc12
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2021-02-07 14:36:55 +0200

    Fix export

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -2,10 +2,12 @@ import React, { FC } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export const Changelog = () => {
+const Changelog = () => {
   return (
     <Layout>
       <SEO title="Netli.fyi Changelog" />
     </Layout>
   )
 }
+
+export default Changelog
